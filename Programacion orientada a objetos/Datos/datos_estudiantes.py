afb705db7c27fc63286ed6b4719d93c43b820879
AlexMeridaPozo <alexmeridagg.mp@gmail.com>
2026-06-29 21:57:02 -0300

    Delete Programacion orientada a objetos directory

--- Programacion orientada a objetos/Datos/datos_estudiantes.py
+++
@@ -1,2 +0,0 @@
-# Archivo de datos generado automáticamente
-ESTUDIANTES = [['T-001', 'Pérez, Lucas', 'Software', '1°', 'Activo'], ['T-004', 'Nuevo Alumno', 'Asignar Carrera', '1°', 'Inactivo']]
--- Programacion orientada a objetos/Datos/datos_tutores.py
+++
@@ -1,4 +0,0 @@
-# Archivo de datos generado automáticamente
-TUTORES = [['T-001', 'Garcia, Maria', 'Matematica', 'Lun/Mie', 'Activo'],
- ['T-003', 'Martinez, Ana', 'Sistemas Operativos', 'Viernes', 'Inactivo'],
- ['T-004', 'Jose Lopez', 'Asignar Materia', '1°', 'Activo']]
